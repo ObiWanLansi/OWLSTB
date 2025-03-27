6cff43bcb4ff9e5cd72c2e59670a6dc3e243ab6f
ObiWanLansi <joerg.lanser@gmail.com>
2025-03-27 16:11:42 +0100

    CommandConfig

--- RPCC/src/RPCCService.py
+++ RPCC/src/RPCCService.py
@@ -1,6 +1,7 @@
 import datetime
 import socket
 import fastapi
+import tomllib
 
 from rich import print
 
@@ -8,8 +9,14 @@ from rich import print
 
 
 HOST = socket.gethostname()
+VERSION = "1.0"
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+def loadConfig() -> dict:
+    with open("commands.toml", "rb") as f:
+        return tomllib.load(f)
 
 
 def execute(command: str) -> dict:
@@ -17,15 +24,17 @@ def execute(command: str) -> dict:
     return {
         "exitcode": "-42",
         "stdout": None
-        }
+    }
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
+topics_and_commands = loadConfig()
+
 app = fastapi.FastAPI()
 app.title = "Raspberry Pi Command & Control"
 app.description = "Remote functions for a raspberry pi which can be called with url's."
-app.version = "1.0"
+app.version = VERSION
 app.contact = {
     "name": "ObiWanLansi",
     "url": "https://github.com/ObiWanLansi/OWLSTB/tree/main/RPCC"
@@ -36,16 +45,27 @@ app.contact = {
 
 @app.get("/status", summary="Return the current status of the server.")
 async def status():
-    return {"running": True, "version": "1.0", "host": HOST, "timestamp": datetime.datetime.now(datetime.timezone.utc)}
+    return {"running": True, "version": VERSION, "host": HOST, "timestamp": datetime.datetime.now(datetime.timezone.utc)}
+
+
+@app.get("/commands")
+async def commands():
+    return topics_and_commands
+
+
+@app.get("/command/{topic}/{command}")
+async def command(topic: str, command: str):
+    return {"topic": topic, "command": command}
+
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-@app.get("/screensaver/activate", summary="Activate the screensaver.")
-async def screensaver_activate():
-    return execute("xscreensaver-command -activate")
+# @app.get("/screensaver/activate", summary="Activate the screensaver.")
+# async def screensaver_activate():
+#     return execute("xscreensaver-command -activate")
 
 
-@app.get("/screensaver/deactivate", summary="Deactivate the screensaver.")
-async def screensaver_deactivate():
-    return execute("xscreensaver-command -deactivate")
+# @app.get("/screensaver/deactivate", summary="Deactivate the screensaver.")
+# async def screensaver_deactivate():
+#     return execute("xscreensaver-command -deactivate")
